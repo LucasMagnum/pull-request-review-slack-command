9f359ad9c9322565219f0c1c06475e3e5244d08b
Lucas Magnum <lucas.magnum@fyndiq.com>
2018-06-18 15:24:41 +0200

    Convert number of reviews to int

--- app/config.py
+++ app/config.py
@@ -9,4 +9,4 @@ ALLOWED_CHANNELS = os.getenv('ALLOWED_CHANNELS', '').split(',')
 ALLOWED_DOMAINS = os.getenv('ALLOWED_DOMAINS', '').split(',')
 
 BLACKLIST_REVIEWERS = os.getenv('BLACKLIST_REVIEWERS', '').split(',')
-NUMBER_OF_REVIEWERS = os.getenv('NUMBER_OF_REVIEWERS', 2)
+NUMBER_OF_REVIEWERS = int(os.getenv('NUMBER_OF_REVIEWERS', 2))
